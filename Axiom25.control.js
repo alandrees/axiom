800776801626282174540cc3d1ad14b0862ad1bf
Alan Drees <alandrees@theselves.com>
2014-09-11 07:18:01 -0400

    Initial commit

---
+++ Axiom25.control.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright 2014 Alan Drees
+ *   
+ * Purpose:
+ *  Implements controller script for an Axiom25 controller
+ *  
+ * Requires:
+ *
+ */
+
+
+
---
+++ Axiom61.control.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright 2014 Alan Drees
+ *   
+ * Purpose:
+ *  Implements controller script for an Axiom25 controller
+ *  
+ * Requires:
+ *
+ */
